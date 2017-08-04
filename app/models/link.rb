5c2f9f0e9e9ad6e97691817a9a2ea994f3ced93b
Victor Allen <vamuigua@gmail.com>
2017-08-04 16:00:59 +0300

    Add relationship btwn user and link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ApplicationRecord
+	#creating relationship btwn link and user
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #create relationship btwn user and link
+  has_many :links
 end
