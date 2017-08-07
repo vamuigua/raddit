d145af105695bb97a8e04fe1b225d7eccf31cf13
Victor Allen <vamuigua@gmail.com>
2017-08-07 17:24:28 +0300

    Added relationship between comment and link

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ApplicationRecord
 	acts_as_votable
 	#creating relationship btwn link and user
 	belongs_to :user
+	has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807102806) do
+ActiveRecord::Schema.define(version: 20170807141528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "link_id"
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -57,4 +67,5 @@ ActiveRecord::Schema.define(version: 20170807102806) do
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
+  add_foreign_key "comments", "users"
 end
