32916c0de0ee33d3d214258c4d6f610c3816481b
Victor Allen <vamuigua@gmail.com>
2017-08-07 13:40:31 +0300

    Added routes for upvote and downvote

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 	#root of app set
 	root 'links#index'
-  resources :links
+	
+  resources :links do
+  	member do
+  		put "like",    to: "links#upvote"
+  		put "dislike", to: "link#downvote" 
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
