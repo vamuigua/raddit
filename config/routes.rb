86eaee51a80d2d22a8ccdc190e51cd80c4d475b2
Victor Allen <vamuigua@gmail.com>
2017-08-07 17:28:31 +0300

    Add comments route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   		put "like",    to: "links#upvote"
   		put "dislike", to: "links#downvote" 
   	end
+    resources :comments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
